64e4e8edac130abdd4400e567655c5d6e859f425
abe-lst92 <abraham.sauz7@gmail.com>
2020-12-30 18:53:47 -0700

    Noo

--- javaS.js
+++ javaS.js
@@ -1,13 +1,20 @@
-
- 
-function myQuiz(){
+(function(){
+  function buildQuiz(){
+    // variable to store the HTML output
     var output = [];
+
+    // for each question...
     myQuestions.forEach(
-        (currentQuestions, questionNumber) => {
-            var answers = [];
+      (currentQuestion, questionNumber) => {
+
+        // variable to store the list of possible answers
+        var answers = [];
+
+        // and for each available answer...
         for(letter in currentQuestion.answers){
-                 // ...add an HTML radio button
-         answers.push(
+
+          // ...add an HTML radio button
+          answers.push(
             `<label>
               <input type="radio" name="question${questionNumber}" value="${letter}">
               ${letter} :
@@ -15,86 +22,95 @@ function myQuiz(){
             </label>`
           );
         }
+
+        // add this question and its answers to the output
         output.push(
-            `<div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div>`
-          );
-    });
+          `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join('')} </div>`
+        );
+      }
+    );
+
+    // finally combine our output list into one string of HTML and put it on the page
     quizContainer.innerHTML = output.join('');
+  }
 
-}
+  function showResults(){
 
-
-
-function showResults(){
+    // gather answer containers from our quiz
     var answerContainers = quizContainer.querySelectorAll('.answers');
-    
+
+    // keep track of user's answers
     let numCorrect = 0;
 
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-       
-        var answerContainer = answerContainers[questionNumber];
-        var selector = `input[name=question${questionNumber}]:checked`;
-        var userAnswer = (answerContainer.querySelector(selector) || {}).value;
-   
-        if(userAnswer === currentQuestion.correctAnswer){
-            // add to the number of correct answers
-            numCorrect++;
-      
-            // color the answers green
-            answerContainers[questionNumber].style.color = 'lightgreen';
-          }
-          // if answer is wrong or blank
-          else{
-            // color the answers red
-            answerContainers[questionNumber].style.color = 'red';
-          };
-   
+    // for each question...
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+      // find selected answer
+      var answerContainer = answerContainers[questionNumber];
+      var selector = `input[name=question${questionNumber}]:checked`;
+      var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if(userAnswer === currentQuestion.correctAnswer){
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      // if answer is wrong or blank
+      else{
+        // color the answers red
+        answerContainers[questionNumber].style.color = 'red';
+      }
     });
+
+    // show number of correct answers out of total
     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-};
+  }
 
-myQuiz();
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
-var questions = [
+  var quizContainer = document.getElementById('quiz');
+  var resultsContainer = document.getElementById('results');
+  var submitButton = document.getElementById('submit');
+  var myQuestions = [
     {
-        question: "Who invented JavaScript?",
-        answers: {
-            a: "Douglas Crockford",
-            b: "Sheryl Sandberg",
-            c: "Brendan Eich"
-        },
-        correctAnswer: "c"
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+      },
+      correctAnswer: "c"
     },
-
     {
-       question: "Which one of these is a JavaScript package manager?",
-       answers: {
-         a: "Node.js",
-         b: "TypeScript",
-         c: "npm"
-       },
-       correctAnswer: "c"
-     },
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        a: "Node.js",
+        b: "TypeScript",
+        c: "npm"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "ESLint"
+      },
+      correctAnswer: "d"
+    }
+  ];
 
-     {
-       question: "Which tool can you use to ensure code quality?",
-       answers: {
-         a: "Angular",
-         b: "jQuery",
-         c: "RequireJS",
-         d: "ESLint"
-       },
-       correctAnswer: "d"
-     }
+  // Kick things off
+  buildQuiz();
 
-
-]
-
-submitButton.addEventListener('click', showResults);
-
+  // Event listeners
+  submitButton.addEventListener('click', showResults);
+})();
+console.log("thisdfosdfadfa")
 
 //A recipe card 
 // create an object to hold information on your favorite recipe. it should have 4
